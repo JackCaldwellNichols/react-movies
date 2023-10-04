07eb4738e7b5d53b01a1c924bdcab093830745cc
PILAR <wer@wers-iMac.local>
2023-10-04 11:48:54 +0200

    backend url

--- ReactMovies/src/main.jsx
+++ ReactMovies/src/main.jsx
@@ -2,18 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { MovieContextProvider } from './Context/MovieContext/MovieContext.jsx'
 import { AuthContextProvider } from './Context/AuthContext/AuthContext.jsx'
+import { MovieContext, MovieContextProvider } from './Context/MovieContext/MovieContext.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthContextProvider>
-     
-    <MovieContextProvider>
+    <AuthContextProvider> 
+      <MovieContextProvider>
       <App />
-    </MovieContextProvider>
-
+      </MovieContextProvider>
     </AuthContextProvider>
   </React.StrictMode>,
 )
